44e3d8dd81a1064162679b3adb357ddb3e50b310
laaronchang <aacidus@gmail.com>
2024-07-15 12:38:40 -0700

    Added Routes

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,7 +4,7 @@ import { PlantsIndex } from "./PlantsIndex";
 import { PlantsNew } from "./PlantsNew";
 import { PlantsShow } from "./PlantsShow";
 import { Modal } from "./Modal";
-
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
   const [plants, setPlants] = useState([]);
@@ -65,14 +65,35 @@ export function Content() {
 
   useEffect(handlePlantsIndex, []);
 
+  // return (
+  //   <div>
+  //     <h1>Welcome to Plants! Content.jsx</h1>
+  //     <PlantsNew onCreatePlant={handleCreatePlant} />
+  //     <PlantsIndex plants={plants} onShowPlant={handleShowPlant} />
+  //     <Modal show={isPlantsShowVisible} onClose={handleClose}>
+  //       <PlantsShow plant={currentPlant} onUpdatePlant={handlUpdatePlant} onDestroyPlant={handleDestroyPlant} />
+  //     </Modal>
+  //   </div>
+  // )
+
   return (
     <div>
-      <h1>Welcome to Plants! Content.jsx</h1>
-      <PlantsNew onCreatePlant={handleCreatePlant} />
-      <PlantsIndex plants={plants} onShowPlant={handleShowPlant} />
+      <Routes>
+        {/* <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<LogoutLink />} /> */}
+        <Route path="/plants/new" element={<PlantsNew />} />
+        <Route path="/plants" element={<PlantsIndex plants={plants} onShowPlant={handleShowPlant} />} />
+      </Routes>
+
       <Modal show={isPlantsShowVisible} onClose={handleClose}>
-        <PlantsShow plant={currentPlant} onUpdatePlant={handlUpdatePlant} onDestroyPlant={handleDestroyPlant} />
+        <h2>{currentPlant.name}</h2>
+        <p>Species: {currentPlant.species}</p>
+        <img src ={currentPlant.plant_image} />
+        <p>Sun: {currentPlant.sun_amount}</p>
+        <p>Water Per Week: {currentPlant.days_water}</p>
+        <p>Description: {currentPlant.description}</p>
       </Modal>
     </div>
-  )
+  );
 }
