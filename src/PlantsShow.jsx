43d3de267f195755c792536e008deed261d17ca9
laaronchang <aacidus@gmail.com>
2024-07-15 11:25:53 -0700

    Added patch option inside Modal

--- src/PlantsShow.jsx
+++ src/PlantsShow.jsx
@@ -8,6 +8,26 @@ export function PlantsShow(props) {
       <p>Sun: {props.plant.sun_amount}</p>
       <p>Water Per Week: {props.plant.days_water}</p>
       <p>Description: {props.plant.description}</p>
+      <form>
+        <div>
+          Name: <input defaultValue={props.plant.name} name="name" type="text" />
+        </div>
+        <div>
+          Species: <input name="species" type="text" />
+        </div>
+        <div>
+          Image Url: <input name="plant_image" type="text" />
+        </div>
+        <div>
+          Amount of Sun: <input name="sun_amount" type="text" />
+        </div>
+        <div>
+          Days to Water: <input name="days_water" type="integer" />
+        </div>
+        <div>
+          Description: <input name="description" type="text" />
+        </div>
+      </form>
     </div>
   );
 }
